6d8de572f1d813e343126d8f91fa7bec9fd44a13
hanane54 <hanane59taouil@gmail.com>
2022-03-02 21:43:01 +0100

    Added: single project item in the projects list

--- src/components/Projects/SingleProject.js
+++ src/components/Projects/SingleProject.js
@@ -1,8 +1,25 @@
+import styles from './SingleProject.module.css';
+import project1 from '../../../src/assets/2.jpg';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 function SingleProject() {
     return (
-        <img src="../2.jpg" alt="project1"/>
+        <>
+            <div className={styles.projectContainer}>
+                <img  className={styles.projectImage} src={project1} alt="project1"/>
+                <figcaption id={styles.projectName}>Mosque Construction</figcaption>
+                <div className={styles.ProgressionSection}>
+                    <ProgressBar 
+                        completed={70} 
+                        bgColor={'#ffc900'}
+                        baseBgColor = {'#fff89a'}
+                        margin = {10}
+                        width = {350}
+                    />
+                    <p className={styles.target} >500$</p>
+                </div>
+            </div>
+        </>
     )
 }
-
 export default SingleProject;
